76e4c585f38a1d155cd9911177c4088add13969b
Ikek102030 <86870673+Ikek102030@users.noreply.github.com>
2021-08-21 18:56:01 +0900

    Update Video.js

--- project/Video/Video.js
+++ project/Video/Video.js
@@ -3,19 +3,12 @@ const intro = document.querySelector('.intro');
 const video = intro.querySelector('video');
 const text = intro.querySelector('h5');
 
-const section = document.querySelector('.scd');
-const end = section.querySelector('h1');
-const video2 = section.querySelector('.video2');
-
-const chapter1 = document.querySelector('.chapter1');
-const video3 = chapter1.querySelector('.video3');
-
 //Scroll magic
 const controller = new ScrollMagic.Controller();
 
 //첫번째 비디오
 let scene = new ScrollMagic.Scene({
-    duration: 5000,
+    duration: 4000,
     triggerElement: intro,
     triggerHook: 0
 })
@@ -26,65 +19,9 @@ let scene = new ScrollMagic.Scene({
 const textAnim = TweenMax.fromTo(text, 3, {opacity: 1}, {opacity: 0});
 
 let scene2 = new ScrollMagic.Scene({
-    duration: 3000,
+    duration: 2000,
     triggerElement: intro,
     triggerHook: 0
 })
     .setTween(textAnim)
     .addTo(controller);
-
-//두번째 비디오
-let scene3 = new ScrollMagic.Scene({
-    duration: 5000,
-    triggerElement: section,
-    triggerHook: 0
-})
-    .setPin(section)
-    .addTo(controller);
-
-//세번째 비디오
-let scene4 = new ScrollMagic.Scene({
-    duration: 7000,
-    triggerElement: chapter1,
-    triggerHook: 0
-})
-    .setPin(chapter1)
-    .addTo(controller);
-
-
-//Video1
-let accelamount = 0.5;
-let scrollpos = 0;
-let delay = 0;
-
-scene.on('update', e => {
-    scrollpos = e.scrollPos / 1000;
-});
-
-setInterval(() => {
-    delay += (scrollpos - delay) * accelamount;
-
-    video.currentTime = delay;
-}, 41.6);
-
-//Video2
-scene3.on('update', e => {
-    scrollpos = e.scrollPos / 1000;
-});
-
-setInterval(() => {
-    delay += (scrollpos - delay) * accelamount;
-
-    video2.currentTime = delay
-}, 41.6);
-
-//Video3
-scene4.on('update', e => {
-    scrollpos = e.scrollPos / 1000;
-});
-
-setInterval(() => {
-    delay += (scrollpos - delay) * accelamount;
-
-    video3.currentTime = delay
-}, 41.6);
